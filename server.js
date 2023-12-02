953734917e5054b22172b82ffbe4576129812348
2hyeong <tubs5840@naver.com>
2023-12-02 23:34:58 +0900

    image classifier

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-// server.js
+// server.js 파일 생성
 
 const express = require('express');
-const app = express();
+const multer  = require('multer');
+const path = require('path');
 
-// CORS 설정
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // 모든 도메인에서 요청 허용
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+const app = express();
+const port = 3000;
+
+// 이미지 업로드를 위한 Multer 설정
+const storage = multer.memoryStorage(); // 이미지를 메모리에 저장
+const upload = multer({ storage: storage });
+
+// 정적 파일 제공 (웹 페이지 및 이미지에 접근하기 위해)
+app.use(express.static('public'));
+
+// 업로드된 이미지를 분류하고 결과를 반환하는 엔드포인트
+app.post('/upload-image', upload.single('image'), (req, res) => {
+    // req.file.buffer에 업로드된 이미지의 데이터가 있음
+    // 이 데이터를 Teachable Machine 모델에 전달하여 결과를 얻어옴
+
+    // 여기에 Teachable Machine 모델을 호출하고 결과를 얻는 코드를 추가
+
+    // 결과를 클라이언트에 반환
+    res.json({ result: 'classification result' });
 });
 
-// 정적 파일 제공 (모델 파일이 위치한 디렉토리)
-app.use(express.static('my_model'));
-
 // 서버 시작
-const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
